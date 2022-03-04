ae43a77ee9c125f59f64b4b33e5eb4b71e9076e2
大久保徹郎 <okubo@office-meet.com>
2022-03-04 15:13:40 +0900

    プロジェクト作成

---
+++ InventoryApp-okubo/InventoryApp-okubo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InventoryApp-okubo
+//
+//  Created by 大久保徹郎 on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InventoryApp-okubo/InventoryApp-okubo/InventoryApp_okuboApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp_okuboApp.swift
+//  InventoryApp-okubo
+//
+//  Created by 大久保徹郎 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp_okuboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
