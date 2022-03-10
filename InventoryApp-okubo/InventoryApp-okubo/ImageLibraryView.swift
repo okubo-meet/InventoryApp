8d7d2d79574738e2c305cd27acab7aa719822829
大久保徹郎 <okubo@office-meet.com>
2022-03-10 14:20:09 +0900

    #1 画像追加に関するsheetを作成

---
+++ InventoryApp-okubo/InventoryApp-okubo/ImageLibraryView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageLibraryView.swift
+//  InventoryApp-okubo
+//
+//  Created by 大久保徹郎 on 2022/03/10.
+//
+
+import SwiftUI
+
+struct ImageLibraryView: View {
+    var body: some View {
+        Text("肉、魚、野菜などの画像をいくつか用意して選択してもらう画面")
+    }
+}
+
+struct ImageLibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageLibraryView()
+    }
+}
