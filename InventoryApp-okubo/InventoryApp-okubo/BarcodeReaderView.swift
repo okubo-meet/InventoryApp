f3a2032a6be9512a035aadb4fa95790ffb2827e6
大久保徹郎 <okubo@office-meet.com>
2022-04-01 13:59:08 +0900

    #5 バーコードを検知して値を出力する処理を追加

--- InventoryApp-okubo/InventoryApp-okubo/BarcodeReaderView.swift
+++ InventoryApp-okubo/InventoryApp-okubo/BarcodeReaderView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AVFoundation
+import Vision
 //バーコードを読み取る画面　楽天APIを使用する予定
 struct BarcodeReaderView: UIViewControllerRepresentable {
     //環境変数で取得したdismissハンドラー
@@ -19,12 +20,36 @@ struct BarcodeReaderView: UIViewControllerRepresentable {
     private let previewLayer = AVCaptureVideoPreviewLayer()
     //ビデオデータ出力のインスタンス
     let videoDataOutput = AVCaptureVideoDataOutput()
+    //メタデータ出力のインスタンス
+    let metaDataOutput = AVCaptureMetadataOutput()
     
     // MARK: - Coordinator
-    class Coordinator: AVCaptureSession {
+    class Coordinator: AVCaptureSession, AVCaptureMetadataOutputObjectsDelegate,
+                       AVCaptureVideoDataOutputSampleBufferDelegate {
         let parent: BarcodeReaderView
         init(_ parent: BarcodeReaderView) {
             self.parent = parent
+        }
+        
+        //新たなビデオフレームが書き込むたびに呼び出されるデリゲートメソッド
+        func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+            // フレームからImageBufferに変換
+            guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+            //バーコード検出用のVisionハンドラ
+            let requestHandler = VNSequenceRequestHandler()
+            //画像内のバーコードを検出するVsionリクエスト
+            let barcodesRequest = VNDetectBarcodesRequest { result, _ in
+                //バーコードがなければ処理なし
+                guard let barcode = result.results?.first as? VNBarcodeObservation else {
+                    return
+                }
+                //読み取ったコードを出力
+                if let value = barcode.payloadStringValue {
+                    print("読み取り：\(value)")
+                }
+            }// VNDetectBarcodesRequest
+            //バーコード検出開始
+            try? requestHandler.perform([barcodesRequest], on: pixelBuffer, orientation: .downMirrored)
         }
     }
     func makeCoordinator() -> Coordinator {
@@ -38,6 +63,12 @@ struct BarcodeReaderView: UIViewControllerRepresentable {
         viewController.view.frame = UIScreen.main.bounds
         setCamera()
         setPreviewLayer()
+        //AVCaptureVideoDataOutputSampleBufferDelegateを呼び出す設定
+        videoDataOutput.setSampleBufferDelegate(context.coordinator, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        //AVCaptureMetadataOutputObjectsDelegateを呼び出す設定
+        metaDataOutput.setMetadataObjectsDelegate(context.coordinator, queue: .main)
+        //映像からメタデータを出力できるよう設定
+        captureSession.addOutput(videoDataOutput)
         //キャプチャーセッション開始
         captureSession.startRunning()
         return viewController
