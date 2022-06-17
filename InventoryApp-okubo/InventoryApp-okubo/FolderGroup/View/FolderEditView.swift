6160de3d50e4dd63b38cb3a782eb3d1c42eabb05
大久保徹郎 <okubo@office-meet.com>
2022-06-17 14:38:27 +0900

    #17 既存のフォルダの編集機能を追加

--- InventoryApp-okubo/InventoryApp-okubo/FolderGroup/View/FolderEditView.swift
+++ InventoryApp-okubo/InventoryApp-okubo/FolderGroup/View/FolderEditView.swift
@@ -13,25 +13,59 @@ struct FolderEditView: View {
     @EnvironmentObject var testData: TestData
     // 環境変数で取得したdismissハンドラー
     @Environment(\.dismiss) var dismiss
+    // 保存済みのフォルダを判別するインデックス番号
+    @Binding var folderIndex: Int?
     // 編集するフォルダ
-    @State var folder: Folder
+    @State var folder = Folder(name: "", isStock: true, icon: Icon.house.rawValue)
+    // フォルダ削除時に表示するアラートのフラグ
+    @State private var showAlert = false
     // MARK: - View
     var body: some View {
         NavigationView {
             Form {
-                TextField("フォルダ名", text: $folder.name)
-                Picker("種類", selection: $folder.isStock, content: {
-                    Text("在庫リスト").tag(true)
-                    Text("買い物リスト").tag(false)
-                })
-                Picker("アイコン", selection: $folder.icon, content: {
-                    ForEach(Icon.allCases, id: \.self) { icon in
-                        Image(systemName: icon.rawValue).tag(icon.rawValue)
-                            .foregroundColor(.orange)
+                Section {
+                    HStack {
+                        Text("フォルダ名")
+                        TextField("入力してください", text: $folder.name)
+                            .textFieldStyle(.roundedBorder)
                     }
-                })
-                // TODO: - 削除ボタンを追加する
-            }
+                    Picker("種類", selection: $folder.isStock, content: {
+                        Text("在庫リスト").tag(true)
+                        Text("買い物リスト").tag(false)
+                    })
+                    Picker("アイコン", selection: $folder.icon, content: {
+                        ForEach(Icon.allCases, id: \.self) { icon in
+                            Image(systemName: icon.rawValue).tag(icon.rawValue)
+                                .foregroundColor(.orange)
+                        }
+                    })
+                }
+                Section {
+                    if folderIndex != nil {
+                        Button(action: {
+                            showAlert = true
+                        }, label: {
+                            HStack {
+                                Image(systemName: "trash.fill")
+                                Text("フォルダを削除する")
+                            }
+                        })
+                        .foregroundColor(.red)
+                    }
+                }
+            }// Form
+            // 削除アラート
+            .alert("フォルダを削除します", isPresented: $showAlert, actions: {
+                Button("削除", role: .destructive) {
+                    if let index = folderIndex {
+                        testData.items.removeAll(where: {$0.folder == folder.name})
+                        testData.folders.remove(at: index)
+                    }
+                    dismiss()
+                }
+            }, message: {
+                Text("このフォルダに保存されたデータも削除されます")
+            })
             .navigationTitle("フォルダ設定")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar(content: {
@@ -45,7 +79,7 @@ struct FolderEditView: View {
                 ToolbarItem(placement: .navigationBarTrailing, content: {
                     Button("保存") {
                         // フォルダ設定保存の処理
-                        if let index = testData.folders.firstIndex(where: {$0.id == folder.id}) {
+                        if let index = folderIndex {
                             // 更新
                             testData.folders[index] = folder
                         } else {
@@ -58,11 +92,20 @@ struct FolderEditView: View {
                 })
             })// toolbar
         }// NavigationView
+        .onAppear {
+            if let index = folderIndex {
+                print("既存のフォルダ： \(testData.folders[index])")
+                folder = testData.folders[index]
+                print("代入:\(folder)")
+            } else {
+                print("新規作成")
+            }
+        }
     }// View
 }
 
 struct FolderEditView_Previews: PreviewProvider {
     static var previews: some View {
-        FolderEditView(folder: .init(name: "", isStock: true, icon: "無し"))
+        FolderEditView(folderIndex: .constant(nil), folder: .init(name: "", isStock: true, icon: "無し"))
     }
 }
--- InventoryApp-okubo/InventoryApp-okubo/FolderGroup/View/FolderView.swift
+++ InventoryApp-okubo/InventoryApp-okubo/FolderGroup/View/FolderView.swift
@@ -16,38 +16,72 @@ struct FolderView: View {
     // フォルダ設定画面の呼び出しフラグ
     @State var showSheet = false
     // フォルダ設定画面に渡すインデックス番号
-    private var folderIndex: Int?
+    @State private var folderIndex: Int?
     // MARK: - View
     var body: some View {
         NavigationView {
             Form {
                 // 在庫リストのフォルダのみ取得
                 let stock = testData.folders.filter({$0.isStock})
-                let buy = testData.folders.filter({$0.isStock == false})
                 // 在庫リストのフォルダ
                 Section {
                     ForEach(stock) { folder in
-                        NavigationLink(destination: ItemListView(folder: folder)) {
-                            HStack {
-                                Image(systemName: folder.icon)
-                                    .foregroundColor(.orange)
-                                Text(folder.name)
-                            }
-                        }// NavigationLink
+                        if isEditing {
+                            Button(action: {
+                                folderIndex = testData.folders.firstIndex(where: {$0.id == folder.id})
+                                showSheet = true
+                            }, label: {
+                                HStack {
+                                    Image(systemName: folder.icon)
+                                        .foregroundColor(.orange)
+                                    Text(folder.name)
+                                        .foregroundColor(.primary)
+                                    Spacer()
+                                    Image(systemName: "folder.badge.gearshape")
+                                        .foregroundColor(.gray)
+                                }
+                            })
+                        } else {
+                            NavigationLink(destination: ItemListView(folder: folder)) {
+                                HStack {
+                                    Image(systemName: folder.icon)
+                                        .foregroundColor(.orange)
+                                    Text(folder.name)
+                                }
+                            }// NavigationLink
+                        }
                     }// ForEach
                 } header: {
                     Text("在庫リスト")
                 }
                 // 買い物リストのフォルダ
+                let buy = testData.folders.filter({$0.isStock == false})
                 Section {
                     ForEach(buy) { folder in
-                        NavigationLink(destination: ItemListView(folder: folder)) {
-                            HStack {
-                                Image(systemName: folder.icon)
-                                    .foregroundColor(.orange)
-                                Text(folder.name)
-                            }
-                        }// NavigationLink
+                        if isEditing {
+                            Button(action: {
+                                folderIndex = testData.folders.firstIndex(where: {$0.id == folder.id})
+                                showSheet = true
+                            }, label: {
+                                HStack {
+                                    Image(systemName: folder.icon)
+                                        .foregroundColor(.orange)
+                                    Text(folder.name)
+                                        .foregroundColor(.primary)
+                                    Spacer()
+                                    Image(systemName: "folder.badge.gearshape")
+                                        .foregroundColor(.gray)
+                                }
+                            })
+                        } else {
+                            NavigationLink(destination: ItemListView(folder: folder)) {
+                                HStack {
+                                    Image(systemName: folder.icon)
+                                        .foregroundColor(.orange)
+                                    Text(folder.name)
+                                }
+                            }// NavigationLink
+                        }
                     }// ForEach
                 } header: {
                     Text("買い物リスト")
@@ -55,7 +89,7 @@ struct FolderView: View {
             }// Form
             .sheet(isPresented: $showSheet, content: {
                 // 設定画面
-                FolderEditView(folder: editFolder())
+                FolderEditView(folderIndex: $folderIndex)
             })
             .navigationTitle("フォルダ")
             .toolbar {
@@ -80,6 +114,7 @@ struct FolderView: View {
                             Spacer()
                             Button("新規フォルダ作成") {
                                 // フォルダ設定画面を呼び出す
+                                folderIndex = nil
                                 showSheet = true
                             }
                         }
@@ -88,15 +123,6 @@ struct FolderView: View {
             }// toolbar
         }// NavigationView
     }// View
-    // MARK: - メソッド
-    // 設定画面に編集するフォルダを渡す関数
-    func editFolder() -> Folder {
-        if let folderIndex = folderIndex {
-            return testData.folders[folderIndex]
-        } else {
-            return Folder(name: "", isStock: true, icon: Icon.house.rawValue)
-        }
-    }
 }
 
 struct FolderView_Previews: PreviewProvider {
